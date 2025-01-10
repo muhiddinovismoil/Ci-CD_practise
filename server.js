4c19550c458b8c0a45cb5600de799eaa8f3d03ff
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-01-10 17:45:07 +0500

    deployment was success

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 2000;
 
 app.use("/", (req, res) => {
-    res.send("Hello World! Welcome This is CI and CD");
+    res.send("Hello World from Ismoil! Welcome This is CI and CD");
 });
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
