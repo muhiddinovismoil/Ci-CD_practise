dc1d581b26e63a34ad6ec724bd0d2d052f44193d
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-01-10 17:14:44 +0500

    bug fixed

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from "express";
 const app = express();
 const PORT = process.env.PORT || 2000;
+
 app.use("/", (req, res) => {
     res.send("Hello World!");
-});
-app.get("/message", (req, res) => {
-    res.send("nima gappp");
 });
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
