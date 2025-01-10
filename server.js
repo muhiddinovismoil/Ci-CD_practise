52725e61aa1b82899ccc99b1a2f8589cceffaee4
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-01-10 17:58:11 +0500

    just test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 1227;
 app.use("/", (req, res) => {
     console.log("hello");
     console.log("hello2");
-    res.send("Hello World from Ismoil! Welcome This is CI and CD");
+    res.send("Assalomu alekum from Ismoil! Welcome This is CI and CD");
 });
 app.listen(PORT, () => {
     console.log(`SERVER is running on PORT: ${PORT}`);
