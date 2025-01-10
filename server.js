48659f0a92a7eb39521696c93a939ac01e816da6
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-01-10 17:42:20 +0500

    deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 2000;
 
 app.use("/", (req, res) => {
-    res.send("Hello World nima gap!");
+    res.send("Hello World! This is CI and CD");
 });
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
