cf315547545a9272dfe0722b53581f0ebd8788d0
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-01-10 17:12:41 +0500

    bug fixed

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 2000;
 app.use("/", (req, res) => {
     res.send("Hello World!");
 });
+app.get("/message", (req, res) => {
+    res.send("nima gappp");
+});
 app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
 });
