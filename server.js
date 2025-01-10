026a305e0d0a576396205a4f9c087d7285f5bd2f
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-01-10 17:49:30 +0500

    restart

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 2000;
 
 app.use("/", (req, res) => {
+    console.log("hello");
     res.send("Hello World from Ismoil! Welcome This is CI and CD");
 });
 app.listen(PORT, () => {
