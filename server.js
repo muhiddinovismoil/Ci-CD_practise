13d7b747f9cf3e7ee41eb1b3d86f20574f9d8750
muhiddinovismoil <muhiddinovismoil2@gmail.com>
2025-01-10 17:52:46 +0500

    just test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 2000;
 
 app.use("/", (req, res) => {
     console.log("hello");
+    console.log("hello2");
     res.send("Hello World from Ismoil! Welcome This is CI and CD");
 });
 app.listen(PORT, () => {
